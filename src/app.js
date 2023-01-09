06b9cf52436a80361f8ef2ccbeb7d79b5c39bd1d
RenatoUbuntu <renatoyferreira@gmail.com>
2023-01-09 15:25:06 -0300

    bugs and fixed

--- src/app.js
+++ src/app.js
@@ -37,14 +37,19 @@ server.post("/tweets", (req, res) => {
 })
 
 server.get("/tweets", (req, res) => {
+    const ultimostweets = []
 
-    tweetlist.forEach((tweet) => {
-        const objeto = userlist.find((user) => user.username == tweet.username)
-        console.log(objeto)
-        tweet.avatar = objeto.avatar
-    })
+    for (let i = 0; i < 10; i++) {
+        if (!tweetlist[i]) return
+        const username = tweetlist[i].username
+        const tweet = tweetlist[i].tweet
+        const userInfo = userlist.find(e => e.username === tweetlist[i].username)
+        const avatar = userInfo.avatar
 
-    res.send(tweetlist.slice(tweetlist.length - 10).reverse())
+        ultimostweets.push({ username: username, avatar: avatar, tweet: tweet })
+    }
+
+    res.send(ultimostweets.reverse());
 })
 
 
